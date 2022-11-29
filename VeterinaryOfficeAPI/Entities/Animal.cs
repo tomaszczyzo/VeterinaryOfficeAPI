1cab19eb9e21e0cca94b6ebc538ef8c3ce2da840
roxiecoal <roxiecoal@DESKTOP-D8IND89>
2022-11-29 15:54:43 +0100

    Dodaj pliki projektów.

---
+++ VeterinaryOfficeAPI/Entities/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeterinaryOfficeAPI.Entities
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Species { get; set; }
+        public string Breed { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public decimal Weight { get; set; }
+        public DateTime FirstVisit { get; set; }
+        public DateTime LastVisit { get; set; }
+
+
+    }
+}
